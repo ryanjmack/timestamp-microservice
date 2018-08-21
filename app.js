bf05a98ef2964b37eb23f25343741b79d7ebbada
Ryan <ryanjmack21@gmail.com>
2018-08-21 12:26:38 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// entry point
+const express = require('express');
+const app     = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Testing!');
+});
+
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
