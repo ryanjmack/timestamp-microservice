49098b6a72ef846b3b4fd9a28ccc5446ec21bb15
root <ryanjmack21@gmail.com>
2018-08-21 20:56:13 -0400

    Add api/timestamp route logic

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // entry point
 const express = require('express');
 const app     = express();
+
 app.use(express.static(__dirname + '/public'));
 
 
@@ -10,14 +11,35 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/api/timestamp/', (req, res) => {
-  res.json({"message": new Date().toString()});
+app.get('/api/timestamp/:dateString?', (req, res) => {
+  let dateString = req.params.dateString;
+  let dateObj;
 
+  if (!dateString) { // no dateString param
+    dateObj = new Date();
+  }
+  else if (/^\d+$/.test(dateString)) { // dateString consists of only digits
+    // new Date takes in milliseconds since epoch though
+    let milliseconds = Number(dateString) * 1000;
+    dateObj = new Date(milliseconds);
+  }
+  else { // could be 'YYYY-MM-DD' format
+    dateObj = new Date(dateString);
+  }
+
+  // If it's a valid date getTime() should not return NaN
+  if (isNaN(dateObj.getTime())) {
+    res.json({"error" : "Invalid Date"});
+  }
+  else {
+    res.json({"unix": dateObj.getTime(), "utc": dateObj.toUTCString()});
+  }
 });
 
 
+// serve index.html on any other route
 app.get('*', (req, res) => {
-  res.status(404).sendFile("index.html", {root: 'public'});
+  res.status(404).sendFile("index.html", {root: __dirname + '/public'});
 });
 
 
